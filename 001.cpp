6bfa75c68b71cc59965ebb480a32d7fd8ac56094
Ringo <eb7usa@gmai.com>
2019-09-06 21:58:51 +0900

    commit

---
+++ 001.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+// output
+#define SPBR(w, n) std::cout<<(w + 1 == n ? '\n' : ' ');
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+// utility
+#define ALL(i) (i).begin(), (i).end()
+#define FOR(i, a, n) for(int i=(a);i<(n);++i)
+#define RFOR(i, a, n) for(int i=(n)-1;i>=(a);--i)
+#define REP(i, n) for(int i=0;i<int(n);++i)
+#define RREP(i, n) for(int i=int(n)-1;i>=0;--i)
+#define IN(a, x, b) (a<=x && x<b)
+#define OUT(a, x, b) (x<a || b<=x)
+template<class T> inline T chmax(T & a, const T b) { return a = (a < b) ? b : a; }
+template<class T> inline T chmin(T& a, const T b) { return a = (a > b) ? b : a; }
+
+// type/const
+#define int ll
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+constexpr int MOD = 1000000007;
+constexpr int INF = 1e18;
+
+using namespace std;
+
+
+signed main() {
+  int N = 1000;
+  N--;
+
+  int fizz = N/3;
+  int buzz = N/5;
+  int fizzbuzz = N/15;
+
+  fizz = 3*fizz*(fizz+1)/2;
+  buzz = 5*buzz*(buzz+1)/2;
+  fizzbuzz = 15*fizzbuzz*(fizzbuzz+1)/2;
+
+  cout << fizz+buzz-fizzbuzz << endl;
+
+  return 0;
+}
