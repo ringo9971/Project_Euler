51f5e6b70cdb51d58c5f1f1f7943f4f6aa6084e3
Ringo <eb7usa@gmai.com>
2020-10-31 18:29:41 +0900

    add

---
+++ cpp/Level2/043.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <complex>
+#include <functional>
+#include <numeric>
+#include <iomanip>
+#include <cassert>
+#include <random>
+#include <chrono>
+
+/* #include <atcoder/all> */
+/* using namespace atcoder; */
+using namespace std;
+
+void debug_out(){ cout << "\n"; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cout << H << " ";
+  debug_out(T...);
+}
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+#define SPBR(w, n) std::cout<<(w + 1 == n ? '\n' : ' ');
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+#define ALL(i) (i).begin(), (i).end()
+#define FOR(i, a, n) for(int i=(a);i<(n);++i)
+#define RFOR(i, a, n) for(int i=(n)-1;i>=(a);--i)
+#define REP(i, n) for(int i=0;i<int(n);++i)
+#define RREP(i, n) for(int i=int(n)-1;i>=0;--i)
+#define IN(a, x, b) (a<=x && x<b)
+#define OUT(a, x, b) (x<a || b<=x)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+#define int ll
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+const ll MOD = 1000000007;
+/* const ll MOD = 998244353; */
+const ll INF = (1<<30)-1;
+const ll INFLL = 1ll<<60;
+const double PI = acos(-1);
+
+struct INIT { INIT(){
+  cin.tie(0); ios::sync_with_stdio(false);
+  cout << fixed << setprecision(10);
+}}INIT;
+
+
+signed main() {
+  vector<int> v(10);
+  iota(ALL(v), 0);
+
+  vector<int> d = {2, 3, 5, 7, 11, 13, 17};
+
+  int ans = 0;
+  do{
+    int now = 0;
+    FOR(i, 1, 10){
+      (now = now*10+v[i]) %= 1000;
+
+      if(i >= 3){
+        if(now%d[i-3] != 0) break;
+        if(i == 9){
+          int ret = 0;
+          REP(j, 10) ret = ret*10+v[j];
+          ans += ret;
+        }
+      }
+    }
+  }while(next_permutation(ALL(v)));
+
+  cout << ans << "\n";
+
+  return 0;
+}
