d0891666a44be6542958a2c194b1e5a25aeffd66
Ringo <eb7usa@gmai.com>
2020-10-31 13:08:55 +0900

    add

---
+++ cpp/Level2/035.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <complex>
+#include <functional>
+#include <numeric>
+#include <iomanip>
+#include <cassert>
+#include <random>
+#include <chrono>
+
+/* #include <atcoder/all> */
+/* using namespace atcoder; */
+using namespace std;
+
+void debug_out(){ cout << "\n"; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cout << H << " ";
+  debug_out(T...);
+}
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+#define SPBR(w, n) std::cout<<(w + 1 == n ? '\n' : ' ');
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+#define ALL(i) (i).begin(), (i).end()
+#define FOR(i, a, n) for(int i=(a);i<(n);++i)
+#define RFOR(i, a, n) for(int i=(n)-1;i>=(a);--i)
+#define REP(i, n) for(int i=0;i<int(n);++i)
+#define RREP(i, n) for(int i=int(n)-1;i>=0;--i)
+#define IN(a, x, b) (a<=x && x<b)
+#define OUT(a, x, b) (x<a || b<=x)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+#define int ll
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+const ll MOD = 1000000007;
+/* const ll MOD = 998244353; */
+const ll INF = (1<<30)-1;
+const ll INFLL = 1ll<<60;
+const double PI = acos(-1);
+
+struct INIT { INIT(){
+  cin.tie(0); ios::sync_with_stdio(false);
+  cout << fixed << setprecision(10);
+}}INIT;
+
+vector<bool> prime(int n){
+  vector<bool> is_prime(n, true);
+  is_prime[0] = is_prime[1] = false;
+  REP(i, n){
+    if(!is_prime[i]) continue;
+    for(int j = 2; i*j < n; j++){
+      is_prime[i*j] = false;
+    }
+  }
+  return is_prime;
+}
+
+signed main() {
+  vector<bool> p = prime(1000001);
+
+  int ans = 0;
+  FOR(i, 2, 1000000){
+    if(!p[i]) continue;
+
+    int now = i;
+    int sz = to_string(i).size();
+    int ten = pow(10, sz);
+    bool flag = true;
+    REP(j, sz-1){
+      now = (now+(now%10)*ten)/10;
+      if(!p[now]){
+        flag = false;
+        break;
+      }
+    }
+    if(flag) ans++;
+  }
+
+  cout << ans << "\n";
+
+  return 0;
+}
